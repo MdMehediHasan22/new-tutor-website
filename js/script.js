191a88e28900b516dfd3b2a24451d02aab7ec08e
MdMehediHasan22 <mdmehedi3799@gmail.com>
2024-12-10 21:03:36 +0600

    complete mobile view section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Toggle mobile menu
 document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.querySelector('.hamburger-button');
-    const mobileMenu = document.querySelector('.mobile-menu');
+    const toggleButton = document.querySelector('.main_navigation .mobile-menu-toggle');
+    const mobileMenu = document.querySelector('.main_navigation .mobile_navigation-items');
   
     toggleButton.addEventListener('click', function () {
       mobileMenu.classList.toggle('active');
